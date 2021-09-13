cf2181e96a266d6eb8863a171d68498d0a3e6623
rawi123 <rawipro.rl@gmail.com>
2021-09-13 15:30:59 +0300

    6.2

---
+++ exercise/6-array/6.2.js
@@ -0,0 +1,31 @@
+const people = ["Greg", "Mary", "Devon", "James"];
+function print1(arr){
+    arr.forEach((i)=>{
+        console.log(i);
+    })
+}
+// people.shift();
+// people.pop();
+// people.unshift("matt");
+// people.push("rawi");
+// people[people.length]="matt";
+// print(people);
+// let i=0;
+// while (i!==-1){
+//     console.log(people[i])
+//     if(people[i]==="Mary")
+//         i=-1;
+//     else i++;
+// }
+// peopleCopy=people.slice(2);
+// print1(peopleCopy);
+
+// console.log(people.indexOf("Mary"),"you may want to add 1");
+// console.log(people.indexOf("foo"));
+
+// print1(people);
+// people.splice(people.indexOf("Devon"),1,"Elizabth","Artie");
+// console.log("shittt");
+// print1(people);
+let withBob=[people,"Bob"];
+console.log(withBob);
