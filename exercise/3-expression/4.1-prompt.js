eb49182979bd64b57c08717ec97eed7aecf9c968
rawi123 <rawipro.rl@gmail.com>
2021-09-13 10:15:50 +0300

    4.1

---
+++ exercise/3-expression/4.1-prompt.js
@@ -0,0 +1,10 @@
+console.log("rawi");
+let siblings=window.prompt("'How many siblings do you have?");
+siblings=parseInt(siblings);
+if(siblings===1){
+    console.log("1 subling!");
+}
+else if(siblings>1){
+    console.log("more than 1 sibling");
+}
+else{ console.log("no siblings");}
