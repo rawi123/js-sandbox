3082879f1de76a4233181fe94e2b8b8c736f151c
rawi123 <rawipro.rl@gmail.com>
2021-09-13 10:19:58 +0300

    4.1

--- exercise/3-expression/4.1-prompt.js
+++ exercise/3-expression/4.1-prompt.js
@@ -8,3 +8,4 @@ else if(siblings>1){
     console.log("more than 1 sibling");
 }
 else{ console.log("no siblings");}
+// triple === also checks for the type of it should use always - else it will give us true for not the same type vars
