7066eabdde4591bc5c7ac20de3f3bab6d055dc26
rawi123 <rawipro.rl@gmail.com>
2021-09-13 09:41:52 +0300

    3.3

---
+++ exercise/3-expression/3.3-functions.js
@@ -0,0 +1,17 @@
+function percentage(population){
+    return ((population/79).toFixed(2));
+}
+let chaina=percentage(1441);
+let israel=percentage(7);
+let swis=percentage(10);
+console.log(chaina);
+console.log(israel);
+console.log(swis);
+
+const pop=num=>(((num/79).toFixed(2)));
+let chaina2=pop(1441);
+let israel2=pop(7);
+let swis2=pop(10);
+console.log(chaina2);
+console.log(israel2);
+console.log(swis2);
