0004a182e8cafc134d686cedac160cb994a876d1
rawi123 <rawipro.rl@gmail.com>
2021-09-19 10:23:59 +0300

    9.1

---
+++ exercise/9-callbacks/9.1.js
@@ -0,0 +1,19 @@
+function isString(str1,call){
+    if(typeof(str1)==="string")
+        call(str1)
+}
+function hehe(str){
+    console.log(str);
+}
+// isString("rawi",hehe)
+const upper=(str,cbfunction)=>{
+    str=(str.split(" ")[0].toUpperCase())+" "+(str.split(" ").slice(1).join(" "))
+    console.log(cbfunction(str));
+}
+const cb=str=>str.split(" ").join("-");
+upper("rawi is the best in this course for sure",cb)
+const mine=(arr,func)=>{
+    func(arr.split(" ").join("||"))
+}
+const func=str=>{console.log(str)}
+mine("hello its us",func)
