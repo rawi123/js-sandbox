1bbc5708063f9c13102cb1e7c8523aa03e2340ff
rawi123 <rawipro.rl@gmail.com>
2021-09-20 12:19:40 +0300

    13.3

--- exercise/13-loops/13.3.js
+++ exercise/13-loops/13.3.js
@@ -1,7 +1,14 @@
 const food = ["Noodle", "Pasta", "Ice-cream", "Meat",
-"Cucumber", "Olives","Ice-cream"];
+"Cucumber", "Olives"];
 const food1 = ["Fries", "Ice-cream", "Pizza", "Olives",
 "Hamburgers"];
+const compare3=(arr1,arr2)=>{
+    arr=arr1.filter((val,i)=>{
+        let temp=arr2.filter(val1=>val===val1)
+        return temp.length
+    })
+    return arr
+}
 const compare=(arr1,arr2)=>{
     let arr=[]
     for (let i=0;i<arr1.length;i++)
@@ -14,4 +21,25 @@ const compare=(arr1,arr2)=>{
         }
     return arr
 }
-console.log(compare(food,food1));
+const compare1=(arr1,arr2)=>{
+    let arr=[]
+    for (let first of arr1){
+        for(let second of arr2){
+            if(first===second)
+            {       if(!arr.includes(second))
+                        arr.push(second)
+                    break
+            }
+        }
+    }
+    return arr
+}
+const compare2=(arr1,arr2)=>{
+    arr=arr1.reduce((temp,val)=>{
+        let temp1=arr2.filter(val1=>val1===val&&!temp.includes(val1))
+        return temp1.length?temp.concat(temp1[0]):temp
+    },[])
+    return arr
+}
+
+console.log(compare3(food,food1));
