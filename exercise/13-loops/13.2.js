1ac77142d2ea27a827b39f51b816506f460be8cd
rawi123 <rawipro.rl@gmail.com>
2021-09-20 11:41:00 +0300

    13.2

---
+++ exercise/13-loops/13.2.js
@@ -0,0 +1,8 @@
+let arr=["boo", "doooo", "hoo","ro"] ;
+let i=0;
+while(i<arr.length){
+    arr[i]=arr[i].length
+    i++
+}
+console.log(arr);
+// i like for loop better because its more orginized in this situation you are going over the array and the while תאני is i smaller than arrlength
