d57b60f9d6208eb1abe4f7d8cee2561667897f97
rawi123 <rawipro.rl@gmail.com>
2021-09-22 00:35:49 +0300

    14.3

---
+++ exercise/14-debugger/14.3.js
@@ -0,0 +1,4 @@
+function getSumOfEven(arr) {
+    return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];//there is no array[10] and return the un eneven
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
