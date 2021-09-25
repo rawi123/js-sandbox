bf1d797671e046c9fb7aa1d4f97ed498d56905ba
rawi123 <rawipro.rl@gmail.com>
2021-09-25 14:40:16 +0300

    weekend final 5

--- weekend project/ex5.js
+++ weekend project/ex5.js
@@ -38,13 +38,55 @@
 
 
 //5.4
-const toWeirdCase = string => (string.split("").map((val, i) => i % 2 === 1 ? val.toLowerCase() : val.toUpperCase()).join(""))
-const toWeirdCase1 = string => {
-    string = string.split("")
-    for (let i = 0; i < string.length; i += 2) {
-        string[i] = string[i].toUpperCase()
-        string[i + 1] ? string[i + 1] = string[i + 1].toLowerCase() : 1
-    }
-    return string.join("")
+// const toWeirdCase = string => (string.split("").map((val, i) => i % 2 === 1 ? val.toLowerCase() : val.toUpperCase()).join(""))
+// const toWeirdCase1 = string => {
+//     string = string.split("")
+//     for (let i = 0; i < string.length; i += 2) {
+//         string[i] = string[i].toUpperCase()
+//         string[i + 1] ? string[i + 1] = string[i + 1].toLowerCase() : 1
+//     }
+//     return string.join("")
+// }
+// console.log(toWeirdCase1("Weird string case"));
+
+//5.5
+// const abbreviate=name=> name.split(" ").map(val=>val[0].toUpperCase()).join(".")
+// function abbreviate2(name){
+//     let str=name[0].toUpperCase()
+//     name=name.match(/ [a-z]/gi).map(val=>val[1].toUpperCase())
+//     return str+"."+name
+// }
+// console.log(abbreviate2("rawi lahiany"));
+
+// const make=str=>{
+//     strArr=str.split("")
+//     return strArr.map((val,i)=>i<strArr.length-4?"#":val).join("")
+// }
+// const mask=string=>{
+//     strArr=string.split("")
+//     for (let i=0;i<strArr.length-4;i++)
+//         strArr[i]="#"
+//     return strArr.join("")
+// }
+// console.log(mask("n!"));
+
+
+//5.7
+const shortest=str=>str.split(" ").sort((a,b)=>a.length-b.length)[0]
+const shortestReduce=str=>{
+    str=str.split(" ")
+    str=str.reduce((shortest,val)=>{
+        return shortest.length>val.length?val:shortest
+    },str[0])
+    return str
 }
-console.log(toWeirdCase1("Weird string case"));
+//5.8
+const longest=str=>str.split(" ").sort((a,b)=>b.length-a.length)[0]
+const longestReduce=str=>{
+    str=str.split(" ")
+    str=str.reduce((longest,val)=>{
+        return longest.length<val.length?val:longest
+    },str[0])
+    return str
+}
+console.log(longestReduce("rawi is the besta"));
