c99b00e054475c6b13a20966d446213b884e5852
rawi123 <rawipro.rl@gmail.com>
2021-09-26 08:56:09 +0300

    ex7

--- weekend project/ex7.js
+++ weekend project/ex7.js
@@ -1,23 +1,22 @@
-// const forFilter = (arr, func) => {
-//     let temp = []
-//     for (let i = 0; i < arr.length; i++) {
-//         let val=arr[i]
-//         if (func(val))
-//             temp.push(val)
-//     }
-//     console.log(temp);
-// }
-// const forMap=(arr,func=0)=>{
-//     for(let i=0;i<arr.length;i++)
-//         arr[i]=func(arr[i])
-//     console.log(arr);
-// }
-// const checkEven=val=>val%2===0
-// const double=val=>val*2
-// const forForEach=(arr,func)=>{
-//     for(let i=0;i<arr.length;i++)
-//         func(arr[i])
-// }
-// let arr = [1, 2, 3, 4]
-// forFilter(arr,checkEven)
-// forMap(arr,double)
+Array.prototype.forFilter = func => {
+    let temp = []
+    for (let i = 0; i < arr.length; i++) {
+        let val=arr[i]
+        if (func(val))
+            temp.push(val)
+    }
+    console.log(temp);
+}
+Array.prototype.forMap=(func=0)=>{
+    for(let i=0;i<arr.length;i++)
+        arr[i]=func(arr[i])
+    console.log(arr);
+}
+const checkEven=val=>val%2===0
+const double=val=>val*2
+Array.prototype.forForEach=func=>{
+    for(let i=0;i<arr.length;i++)
+        func(arr[i])
+}
+let arr = [1, 2, 3, 4]
+arr.forMap(double)
