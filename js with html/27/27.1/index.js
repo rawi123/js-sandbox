7934d48cebd857ff7efe2d57c4967ec84cd39ed9
rawi123 <rawipro.rl@gmail.com>
2021-10-03 15:36:46 +0300

    27.1 with class

--- js with html/27/27.1/index.js
+++ js with html/27/27.1/index.js
@@ -1,12 +1,25 @@
-function Square(a, b, c, d) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.d = d;
+// function Square(a, b, c, d) {
+//     this.a = a;
+//     this.b = b;
+//     this.c = c;
+//     this.d = d;
 
-}
-Square.prototype.equal=function(square){
-    return this.a===square.a&&this.b===square.b&&this.c==square.c&&this.d===square.d
+// }
+// Square.prototype.equal=function(square){
+//     return this.a===square.a&&this.b===square.b&&this.c==square.c&&this.d===square.d
+// }
+
+
+class Square{
+    constructor(a, b, c, d){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+    equal(square){
+        return this.a===square.a&&this.b===square.b&&this.c==square.c&&this.d===square.d
+    }
 }
 const square1=new Square(1,2,3,4)
 // console.log(square1);
