00dbb67c0c9d377e2f092194cb9287227a68d3c0
rawi123 <rawipro.rl@gmail.com>
2021-10-03 14:07:58 +0300

    26.2

--- js with html/26/26.2/index.js
+++ js with html/26/26.2/index.js
@@ -1,8 +1,19 @@
-const storm = {
-    superPower:"invisability",
-    func:printSuperPower
-}
+// function storm() {//option
+//     this.superPower="invisability"
+// }
+// let storm1=new storm()
+// storm.prototype.prinPowers=function(){
+//     this.print=printSuperPower
+//     this.print()
+// }
+// console.log(storm1.prinPowers());
+
 function printSuperPower() {
     console.log("my superpower is " + this.superPower);
+}
+
+const storm={//option 1
+    superPower:"raining",
+    func:printSuperPower
 }
 storm.func();
