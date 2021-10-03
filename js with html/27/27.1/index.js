5ddce14965f5ed3dbc85d33724ac14ec1ddde3b3
rawi123 <rawipro.rl@gmail.com>
2021-10-03 15:21:25 +0300

    27.1

---
+++ js with html/27/27.1/index.js
@@ -0,0 +1,13 @@
+function Square(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+
+}
+Square.prototype.equal=function(square){
+    return this.a===square.a&&this.b===square.b&&this.c==square.c&&this.d===square.d
+}
+const square1=new Square(1,2,3,4)
+// console.log(square1);
+console.log(square1.equal(new Square(1,2,3,4)));
