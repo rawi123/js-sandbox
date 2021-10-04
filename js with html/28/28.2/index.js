d8d8e970e81320a04d5a7ab5afc8e15e8b34c2a1
rawi123 <rawipro.rl@gmail.com>
2021-10-04 15:15:53 +0300

    28.2 and 29.2

---
+++ js with html/28/28.2/index.js
@@ -0,0 +1,32 @@
+const obj = {
+    name: "rawi",
+    printName: function () {
+        console.log(`my name is ${this.name}`);
+    },
+}
+const obj2={
+    name:"samer"
+}
+// const printNameAfterSecond=()=>{
+//     new Promise((reslove, reject) => {
+//         setTimeout(() => {
+//             reslove(obj.printName.call(obj2))
+//         }, 1000);
+//     })//removed then because of the function print name actually logs it so when it reolves it calls the function
+//     .catch((err)=>{
+//         console.log("error occured");
+//     })
+// }
+const printNameAfterSecond=()=>{
+    new Promise((reslove, reject) => {
+        setTimeout(() => {
+            reslove(obj.printName.bind(obj2))
+        }, 1000);
+    }).then((res)=>{
+        res()
+    })
+    .catch((err)=>{
+        console.log("error occured");
+    })
+}
+printNameAfterSecond()
